b5b4fc5ae893f83576d476fe3d5dcdb93d97cb38
devcontenidosinteractivos <98177573+AndresDiazPinto@users.noreply.github.com>
2025-04-30 20:06:29 -0500

    Initial commit
    Se realiza banner, intro, sintesis y json

--- src/config/global.js
+++ src/config/global.js
@@ -1,17 +1,22 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    Name: 'Reporte de resultados',
+    Description:
+      'El componente formativo orienta la recolección, registro, análisis y presentación de variables ambientales en agroecosistemas. Incluye tipos de registros, construcción de formatos, informes técnicos, normas vigentes (como la Resolución 631 de 2015), herramientas ofimáticas y criterios de redacción. Su finalidad es generar reportes válidos para la toma de decisiones, seguimiento ambiental y certificaciones ecológicas.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.png'),
       },
       {
         clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.png'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.png'),
       },
     ],
   },
@@ -31,12 +36,13 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Registros para la medición de variables ambientales',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo:
+              'Tipos de registros para la medición de variables ambientales',
             hash: 't_1_1',
           },
         ],
@@ -45,14 +51,20 @@ export default {
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Informes de Medición',
         desarrolloContenidos: true,
-      },
-      {
-        nombreRuta: 'tema3',
-        numero: '3',
-        titulo: 'Tema 3',
-        desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Aspectos básicos de redacción del informe',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Herramientas ofimáticas para elaboración del informe',
+            hash: 't_2_2',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -102,22 +114,75 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: 'Sitio web',
-      link: '',
+      tema: 'Registros para la medición de variables ambientales',
+      referencia:
+        'SENA. (2022). Tipo de registros para la medición de variables ambientales. [Archivo de video] Youtube.  ',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=k9XUkgCGnGI ',
+    },
+    {
+      tema: 'Informes de medición',
+      referencia:
+        'Fuentes, C. (2003). Redacción y presentación de informes. Revistas de Ciencias Administrativas y Financieras de la Seguridad Social, 11 (2), 75-85.',
+      tipo: 'Artículo',
+      link:
+        'https://www.scielo.sa.cr/scielo.php?script=sci_arttext&pid=S1409-12592003000200007&lng=en&tlng=es ',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Diligenciamiento',
+      significado:
+        'procesos en el que se rellenan los formatos o planillas con la información, datos o resultados de la medición de variables ambientales en agroecosistemas.',
+    },
+    {
+      termino: 'Formatos para muestreo',
+      significado: 'instrumentos ',
+    },
+    {
+      termino: 'Informe de medición',
+      significado:
+        'reporte escrito con la descripción del proceso de recolección de la información a través del plan de muestreo, los resultados de la medición y conclusiones útiles para la toma de decisiones acerca del plan de manejo del agroecosistema.',
+    },
+    {
+      termino: 'Recolección de datos',
+      significado:
+        'proceso en el que se obtienen los resultados de la toma y procesamiento de muestras del suelo y del agua, y de la identificación de los bioindicadores del agroecosistema.',
+    },
+    {
+      termino: 'Registro',
+      significado:
+        'formato o planilla debidamente diligenciado y se constituye como documento que evidencia un resultado obtenido o una actividad desarrollada, por lo tanto, no está sujeta a cambios.',
+    },
+    {
+      termino: 'Resultado de la medición',
+      significado:
+        'información procesada a partir de los registros diligenciados de la medición de las variables ambientales.',
+    },
+    {
+      termino: 'Variables ambientales:',
+      significado:
+        'representación cualitativa o cuantitativa asignada a un aspecto ambiental, que permite observar algún tipo de variación al realizar la medición. el propósito de la medición es conocer la afectación o impacto de las actividades productivas del agroecosistema sobre el medio ambiente y cómo estas variables pueden afectar a otras con las que están relacionadas.',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Fuentes, C. (2003). Redacción y presentación de informes. Revistas de Ciencias Administrativas y Financieras de la Seguridad Social, 11(2), 75-85. ',
+      link:
+        'http://www.scielo.sa.cr/scielo.php?script=sci_arttext&pid=S1409-12592003000200007&lng=en&tlng=es ',
+    },
+    {
+      referencia:
+        'IDEAM. (2021). Protocolo de monitoreo y seguimiento del agua.',
+      link:
+        ' https://www.ideam.gov.co/sala-de-prensa/informes/publicacion-vie-23082024-1200-0',
+    },
+    {
+      referencia:
+        'Ministerio de Ambiente y Desarrollo Sostenible. (2015). Resolución 631 de 2015. Por la cual se establecen los parámetros y los valores límites permisibles en los vertimientos puntuales a cuerpos de aguas superficiales y a los sistemas de alcantarillado público y se dictan otras disposiciones.',
+      link:
+        'https://www.minambiente.gov.co/wp-content/uploads/2021/11/resolucion-631-de-2015.pdf',
     },
   ],
   creditos: [
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,6 +8,24 @@
           i.fas.fa-info
       h1 Introducción
 
+    .bg-full-width-1.bg-fondo-1.mb-5
+      .px-4.p-md-5
+        .row.mb-3
+          .col-lg-3.order-2
+            figure
+              img(src="@/assets/curso/intro/1.png", data-aos="zoom-in")
+          .col-lg-9.order-1.mb-3.mb-lg-0
+            p(data-aos="fade-left") La medición de variables ambientales en agroecosistemas es una herramienta fundamental para evaluar el estado del entorno y determinar los efectos de las prácticas agrícolas sobre el suelo, el agua y la biodiversidad. Esta información se recopila mediante registros que permiten documentar datos precisos y verificables sobre distintas condiciones ambientales, cumpliendo con protocolos técnicos establecidos. Dichos registros son la base para elaborar informes que facilitan la toma de decisiones en los planes de manejo sostenible.
+        
+            
+            .row.align-items-start(data-aos="fade-left")
+              .col-lg-5.me-0.pe-0
+                img(src="@/assets/curso/intro/2.png", data-aos="zoom-in")
+              .col-lg-7.bg-color-1.p-4.ms-0.h-100
+                p.mb-1 El adecuado diligenciamiento de los registros garantiza que la información recolectada sea útil, veraz y trazable. Existen distintos tipos de registros según el tipo de variable, la frecuencia de medición y el medio en que se recopilan los datos (físico, digital o mixto). Además, los formatos deben adaptarse a las características del agroecosistema y a los objetivos del estudio. Por lo tanto, la construcción y uso de estos registros exige un enfoque técnico y sistemático.
+        
+        p Una vez recopilados los datos, es esencial organizarlos y analizarlos adecuadamente para presentarlos en informes técnicos. Estos informes no solo describen los resultados, sino que permiten identificar patrones, evaluar impactos, cumplir con normativas vigentes y formular recomendaciones basadas en evidencia. El uso de herramientas ofimáticas y una redacción coherente y estructurada son claves para comunicar los hallazgos de forma clara y efectiva.
+
 </template>
 
 <script>
